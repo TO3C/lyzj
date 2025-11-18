a0f2fde25b867d6784472888063d1e62e5e318cb
您的Gitee用户名 <Gitee>
2025-11-18 14:18:17 +0800

    配置GitHub Pages部署和Actions工作流

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/流云/',
   server: {
     proxy: {
       '/api': {
@@ -11,5 +12,9 @@ export default defineConfig({
         changeOrigin: true
       }
     }
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
   }
 })
